973685ea2d6ad305c9a0019bd84f3606ac06a037
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-10-06 23:18:32 +0300

    responsible navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,70 @@
 import { NextPage } from "next";
 import styles from "../styles/navbar/Navbar.module.scss";
+import Link from "next/link";
+
+import Logo from "../public/assets/icons/logo_black/100.png";
+import SearchIcon from "../public/assets/navbarIcons/SearchIcon";
+import ShopIcon from "../public/assets/navbarIcons/ShopIcon";
 import CartIcon from "../public/assets/navbarIcons/CartIcon";
 import ProfileIcon from "../public/assets/navbarIcons/ProfileIcon";
 import NightIcon from "../public/assets/navbarIcons/NightIcon";
 import DayIcon from "../public/assets/navbarIcons/DayIcon";
+// import BurgerIcon from "../public/assets/navbarIcons/BurgerIcon";
+import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
 const Navbar: NextPage = () => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo_container}>
-        <div>LOGO IMG</div>
-        <div>LOGO</div>
+    <nav className={styles.navbar}>
+      <div className={`${styles.left_container}  ${styles.flex_container}`}>
+        <Link href="/">
+          <div className={styles.logo_container}>
+            <Image
+              src={Logo}
+              alt="logo"
+              width="50"
+              height="50"
+              layout="fixed"
+            />
+            <p className={styles.logo_name}>Shopname</p>
+          </div>
+        </Link>
+        <Link href="/shop">
+          <div className={styles.catalogue_container}>
+            <ShopIcon className={styles.icon} />
+            <p>Shop</p>
+          </div>
+        </Link>
       </div>
-      <div className={styles.searchbar_container}>
+
+      <div className={styles.middle_container}>
+        <SearchIcon className={styles.icon} />
         <input placeholder="SEARCHFIELD" className={styles.searchfield} />
         <button className={styles.search_button}>FIND</button>
       </div>
-      <div className={styles.profile_cart_container}>
+
+      <div className={`${styles.right_container} ${styles.flex_container}`}>
         <div className={styles.change_theme_container}>
-          <NightIcon className={styles.profile_cart_icon} />
-          <DayIcon className={styles.profile_cart_icon} />
+          <NightIcon className={styles.icon} />
+          <DayIcon className={styles.icon} />
         </div>
-        <CartIcon className={styles.profile_cart_icon} />
-        <ProfileIcon className={styles.profile_cart_icon} />
+
+        <Link href="/cart">
+          <CartIcon className={styles.icon} />
+        </Link>
+
+        <Link href="profile">
+          <ProfileIcon className={styles.icon} />
+        </Link>
+
+        <BurgerIcon />
       </div>
-    </div>
+    </nav>
   );
+};
+
+const BurgerIcon = () => {
+  return <div className={styles.burger}></div>;
 };
 
 export default Navbar;
