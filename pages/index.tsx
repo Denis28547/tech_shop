89c426158a5142ec07037bc902d0318dcd2611ed
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-10-29 22:54:23 +0300

    cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,35 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
-import { signIn, signOut, useSession } from "next-auth/react";
-import styles from "../styles/test.module.scss";
-import { useState } from "react";
-import Head from "next/head";
+import halloweenImage from "../public/static/halloween_sale.jpg";
+
+import ItemsGridComponent from "../components/ItemsGridComponent";
 
 const Home: NextPage = () => {
-  const session = useSession();
   const router = useRouter();
 
-  if (session.status === "loading") return <div>LOADING...</div>;
-  return null;
+  // if (session.status === "loading") return <div>LOADING...</div>;
+  return (
+    <>
+      <div
+        className="main_wrapper"
+        style={{ maxWidth: "1200px", margin: "auto" }}
+      >
+        <Image
+          alt="user icon"
+          src={halloweenImage}
+          // width={1200}
+          layout="responsive"
+          style={{ padding: "100px" }}
+        />
+        <h1>Newest items</h1>
+        <ItemsGridComponent />
+        <h1>Sells in your city</h1>
+        <ItemsGridComponent />
+      </div>
+    </>
+  );
 };
 
 export default Home;
