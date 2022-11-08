d0a9021c0b0a115b9682168472323c81784e1692
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-11-08 22:51:12 +0200

    started on implementing redux toolkit, added store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,18 @@ import "../styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { store } from "../store/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    //@ts-ignore
+    // @ts-ignore
     <SessionProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </SessionProvider>
   );
 }
