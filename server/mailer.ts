6e7ee0d9604fc46a6dc03a59a9d250b57391a655
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2023-02-12 19:28:13 +0200

    trying to fix images saving

--- server/mailer.ts
+++ server/mailer.ts
@@ -15,7 +15,84 @@ const sendActivationMail = async (email: string, link: string) => {
       from: "sendmails256@gmail.com",
       to: email,
       subject: "Verification",
-      text: "Hello world?",
+      text: `<center
+      style="
+        width: 100%;
+        table-layout: fixed;
+        background-color: #e7e7e7;
+        padding: 70px 0;
+      "
+    >
+      <table
+        style="
+          background-color: #eeeeee;
+          border: 2px solid #c5c4c4;
+          border-radius: 5px;
+          border-spacing: 0;
+          margin: 0 auto;
+          width: 100%;
+          max-width: 600px;
+          padding: 70px 0;
+        "
+        width="100%"
+      >
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td class="two-columns" style="text-align: center">
+                  <table
+                    class="column"
+                    style="
+                      width: 100%;
+                      max-width: 300px;
+                      display: inline;
+                      vertical-align: top;
+                    "
+                  >
+                    <tr>
+                      <td>
+                        <img src="https://i.ibb.co/N9WJJSp/logo-for-mail.png" alt="logo-for-mail" border="0">
+                      </td>
+                    </tr>
+                  </table>
+                  <table
+                    class="column"
+                    style="
+                      width: 100%;
+                      max-width: 300px;
+                      display: inline;
+                      vertical-align: top;
+                    "
+                  >
+                    <tr>
+                      <td>
+                        <h2 style="margin: 0; height: 28px; padding: 16px 0">
+                          TechShop
+                        </h2>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+  
+        <tr>
+          <td style="text-align: center">
+            <h2 style="margin: 0 0 5px 0">Verification letter</h2>
+          </td>
+        </tr>
+        <tr>
+          <td style="text-align: center">
+            <a style="margin: 0" href=${link}>
+              Please click on this text to verify your e-mail address
+            </a>
+          </td>
+        </tr>
+      </table>
+    </center>`,
       html: `<center
       style="
         width: 100%;
